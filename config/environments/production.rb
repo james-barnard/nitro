ba23fec4c39ec9abf7b0691189c8fef47c5b1d8e
James Barnard <James.barnard@irco.com>
2016-07-30 17:52:04 -0600

    production email using send grid for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,13 +72,13 @@ Rails.application.configure do
   config.active_support.deprecation = :notify
 
   config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
+    address: "smtp.sendgrid.net",
     port: 587,
     domain: Rails.application.secrets.domain_name,
     authentication: "plain",
     enable_starttls_auto: true,
-    user_name: Rails.application.secrets.email_provider_username,
-    password: Rails.application.secrets.email_provider_password
+    email_provider_username: Rails.application.secrets.email_provider_username,
+    email_provider_password: Rails.application.secrets.email_provider_password
   }
   # ActionMailer Config
   config.action_mailer.default_url_options = { :host => Rails.application.secrets.domain_name }
