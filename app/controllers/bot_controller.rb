cf892fb041cf0cb6d02b605952ee8edcc0d4a1f4
James Barnard <James.barnard@irco.com>
2016-08-16 11:15:45 -0600

    test for params hub

--- app/controllers/bot_controller.rb
+++ app/controllers/bot_controller.rb
@@ -1,12 +1,10 @@
 class BotController < ApplicationController
   def hello
     puts "DEBUG: #{params.inspect}"
-    head :ok, content_type: "text/html"
-    #
-   #if params[‘hub.verify_token’] == “beehive.sceptre.oncolog”
-     #render text: params[‘hub.challenge’] and return
-   #else
-     #render text: ‘error’ and return
-   #end
+   if params['hub'] && params[‘hub.verify_token’] == “beehive.sceptre.oncolog”
+     render text: params[‘hub.challenge’] and return
+   else
+     render text: 'error' and return
+   end
   end
 end
