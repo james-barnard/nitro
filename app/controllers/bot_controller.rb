f8c52cc5ed2d47532366b10e6d8b7fc6ee16f697
James Barnard <James.barnard@irco.com>
2016-08-16 11:20:10 -0600

    use the right quotes

--- app/controllers/bot_controller.rb
+++ app/controllers/bot_controller.rb
@@ -1,8 +1,8 @@
 class BotController < ApplicationController
   def hello
     puts "DEBUG: #{params.inspect}"
-   if params['hub'] && params[‘hub.verify_token’] == “beehive.sceptre.oncolog”
-     render text: params[‘hub.challenge’] and return
+    if params['hub.verify_token'] == 'beehive.sceptre.oncolog'
+     render text: params['hub.challenge'] and return
    else
      render text: 'error' and return
    end
