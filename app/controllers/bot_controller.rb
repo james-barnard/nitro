2fc715a1aeb7575fdc800a3eb1af76b62f03aa27
James Barnard <James.barnard@irco.com>
2016-08-16 10:46:06 -0600

    bot controller with routes

---
+++ app/controllers/bot_controller.rb
@@ -0,0 +1,10 @@
+class BotController < ApplicationController
+  def hello
+    puts "DEBUG: #{params.inspect}"
+   if params[‘hub.verify_token’] == “beehive.sceptre.oncolog”
+     render text: params[‘hub.challenge’] and return
+   else
+     render text: ‘error’ and return
+   end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'bot/hello'
   get 'particle/enable'
   get 'particle/buy'
 
