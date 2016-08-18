1c3354fdefc3e00178e50cbb6ffd4564e29a15bd
James Barnard <James.barnard@irco.com>
2016-08-18 17:15:33 -0600

    bot/chat is the target for get and post

--- app/controllers/bot_controller.rb
+++ app/controllers/bot_controller.rb
@@ -1,7 +1,10 @@
 class BotController < ApplicationController
-  def hello
+  @page_access_token = 'EAAY4gi2TZC6QBAGECh9GqOLOC1vqeeqFoCTFNIHTVRL0CAZC9OCABAcXJprfEnSmkNZAXesabT6kGNYsWmUHG7gZA0hVhbGDJ76rppz6OeTXSssNTGY2rG7ECnaaxjavr9UfojSvPnEDwFoOdr3oyAwXuCobatYh1X60mHiZA8wZDZD'
+
+  def chat
     puts "DEBUG: #{params.inspect}"
-    if params['hub.verify_token'] == 'beehive.sceptre.oncolog'
+
+   if params['hub.verify_token'] == 'beehive.sceptre.oncolog'
      render text: params['hub.challenge'] and return
    else
      render text: 'error' and return
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'bot/hello'
+  match 'bot/chat', via: [:get, :post]
+
   get 'particle/enable'
   get 'particle/buy'
 
