0025030e170ba2cae65c907a0cebd934fd822bd2
James Barnard <James.barnard@irco.com>
2016-08-18 18:09:52 -0600

    re-simplify the controller

--- app/controllers/messenger_controller.rb
+++ app/controllers/messenger_controller.rb
@@ -1,10 +1,7 @@
 class MessengerController < Messenger::MessengerController
   def webhook
     puts "Messenger:DEBUG: #{params.inspect}"
-    if params['hub.verify_token'] == 'beehive.sceptre.oncolog'
-     render text: params['hub.challenge'] and return
-    else
-     render text: 'error' and return
-    end
+    #logic here
+    render nothing: true, status: 200
   end
 end
