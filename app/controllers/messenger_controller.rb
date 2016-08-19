2b2d8ad916ab3ce64ddbd345993cd2d61026aeea
James Barnard <James.barnard@irco.com>
2016-08-18 18:19:26 -0600

    send some text back

--- app/controllers/messenger_controller.rb
+++ app/controllers/messenger_controller.rb
@@ -2,6 +2,13 @@ class MessengerController < Messenger::MessengerController
   def webhook
     puts "Messenger:DEBUG: #{params.inspect}"
     #logic here
+    Messenger::Client.send(
+        Messenger::Request.new(
+          Messenger::Elements::Text.new(text: 'we are on a roll!'),
+          fb_params.first_entry.sender_id
+        )
+      )
+    #
     render nothing: true, status: 200
   end
 end
