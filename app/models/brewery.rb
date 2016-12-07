4781e2126e9b453abab2a24aa1d786793f6cba8f
James Barnard <james.barnard.co@gmail.com>
2016-12-07 14:06:02 -0600

    added spaces to forms and names to breweries, locations, products, and people

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,4 +1,5 @@
 class Brewery < ActiveRecord::Base
   belongs_to :region
   belongs_to :person
+  has_many :products, through: :local_products
 end
--- app/models/product_load.rb
+++ app/models/product_load.rb
@@ -1,4 +1,8 @@
 class ProductLoad < ActiveRecord::Base
   belongs_to :local_product
   belongs_to :vending_machine
+
+  def expiration
+    date_loaded + 2.weeks
+  end
 end
