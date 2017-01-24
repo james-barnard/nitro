53141f76b7d25b13fa008b6e52536718c61c9ae6
James Barnard <jwbarnard@comcast.net>
2017-01-24 10:00:54 -0600

    monkey patch 5

--- app/controllers/messenger_controller.rb
+++ app/controllers/messenger_controller.rb
@@ -3,7 +3,7 @@ class MessengerController < Messenger::MessengerController
   include BotLevelOne
 
   def webhook
-    @fbuser = Messenger::FbUser.find_or_create_by(sender_id: sender_id)
+    @fbuser = FbUser.find_or_create_by(sender_id: sender_id)
 #    @part = create_part(fb_params.first_entry.callback)
 
 #    identify_user(fb_params.first_entry.callback) unless identified?
