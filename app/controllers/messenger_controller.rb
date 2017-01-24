74456a8a3912d4fc1db24ae17c408fcb2c66100d
James Barnard <jwbarnard@comcast.net>
2017-01-24 12:33:09 -0600

    full meal deal

--- app/controllers/messenger_controller.rb
+++ app/controllers/messenger_controller.rb
@@ -8,34 +8,34 @@ class MessengerController < Messenger::MessengerController
 
     identify_user(fb_params.first_entry.callback) unless identified?
 
-    request = #if @fbuser.ungreeted?
+    request = if @fbuser.ungreeted?
       request_text(greeting)
-#    elsif not_located?
-#      location_prompt
-#    elsif fb_params.first_entry.callback.postback?
-#      case fb_params.first_entry.callback.payload
-#      when /menu/
-#        fb_request(menu)
-#      when /BrewM1/i
-#        fb_request(enable(:brew1))
-#      when /BrewM2/i
-#        fb_request(enable(:brew2))
-#      else
-#        fb_request(choices1)
-#      end
-#    elsif fb_params.first_entry.callback.message?
-#      case fb_params.first_entry.callback.text
-#      when /meter/i
-#        request_text(meter(fb_params))
-#      when /hello/i
-#        request_text("Hello!")
-#        #location_prompt
-#      when /buy/i
-#        fb_request(choices2)
-#      end
-#    else
-#      request_text(fb_params.inspect)
-#    end
+    elsif not_located?
+      location_prompt
+    elsif fb_params.first_entry.callback.postback?
+      case fb_params.first_entry.callback.payload
+      when /menu/
+        fb_request(menu)
+      when /BrewM1/i
+        fb_request(enable(:brew1))
+      when /BrewM2/i
+        fb_request(enable(:brew2))
+      else
+        fb_request(choices1)
+      end
+    elsif fb_params.first_entry.callback.message?
+      case fb_params.first_entry.callback.text
+      when /meter/i
+        request_text(meter(fb_params))
+      when /hello/i
+        request_text("Hello!")
+        #location_prompt
+      when /buy/i
+        fb_request(choices2)
+      end
+    else
+      request_text(fb_params.inspect)
+    end
 
     Messenger::Client.send( request || request_text("Hello"))
 
