44d6b8d6d67fcbfe10f8290f5369b6baa8be8fd7
James Barnard <jwbarnard@comcast.net>
2017-01-24 13:42:36 -0600

    restart

--- app/models/fb_user.rb
+++ app/models/fb_user.rb
@@ -1,7 +1,7 @@
 class FbUser < ActiveRecord::Base
   has_many :parts
-  CONVERSATION_TIMEOUT=360
-  LOCATION_TIMEOUT=900
+  CONVERSATION_TIMEOUT = 360
+  LOCATION_TIMEOUT     = 900
 
   def ungreeted?
     (Time.now - parts.last.created_at) > CONVERSATION_TIMEOUT
