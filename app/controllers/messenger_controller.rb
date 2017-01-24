7e914731a3efd32e5144c12bcb4a12df95f75654
James Barnard <jwbarnard@comcast.net>
2017-01-23 18:32:15 -0600

    reverting

--- app/controllers/messenger_controller.rb
+++ app/controllers/messenger_controller.rb
@@ -3,39 +3,39 @@ class MessengerController < Messenger::MessengerController
   include BotLevelOne
 
   def webhook
-    @fbuser = FbUser.find_or_create_by(sender_id: sender_id)
-    @part = create_part(fb_params.first_entry.callback)
+#    @fbuser = FbUser.find_or_create_by(sender_id: sender_id)
+#    @part = create_part(fb_params.first_entry.callback)
 
-    identify_user(fb_params.first_entry.callback) unless identified?
+#    identify_user(fb_params.first_entry.callback) unless identified?
 
-    request = if @fbuser.ungreeted?
+    request = #if @fbuser.ungreeted?
       request_text(greeting)
-    elsif not_located?
-      location_prompt
-    elsif fb_params.first_entry.callback.postback?
-      case fb_params.first_entry.callback.payload
-      when /menu/
-        fb_request(menu)
-      when /BrewM1/i
-        fb_request(enable(:brew1))
-      when /BrewM2/i
-        fb_request(enable(:brew2))
-      else
-        fb_request(choices1)
-      end
-    elsif fb_params.first_entry.callback.message?
-      case fb_params.first_entry.callback.text
-      when /meter/i
-        request_text(meter(fb_params))
-      when /hello/i
-        request_text("Hello!")
-        #location_prompt
-      when /buy/i
-        fb_request(choices2)
-      end
-    else
-      request_text(fb_params.inspect)
-    end
+#    elsif not_located?
+#      location_prompt
+#    elsif fb_params.first_entry.callback.postback?
+#      case fb_params.first_entry.callback.payload
+#      when /menu/
+#        fb_request(menu)
+#      when /BrewM1/i
+#        fb_request(enable(:brew1))
+#      when /BrewM2/i
+#        fb_request(enable(:brew2))
+#      else
+#        fb_request(choices1)
+#      end
+#    elsif fb_params.first_entry.callback.message?
+#      case fb_params.first_entry.callback.text
+#      when /meter/i
+#        request_text(meter(fb_params))
+#      when /hello/i
+#        request_text("Hello!")
+#        #location_prompt
+#      when /buy/i
+#        fb_request(choices2)
+#      end
+#    else
+#      request_text(fb_params.inspect)
+#    end
 
     Messenger::Client.send( request || request_text("Hello"))
 
