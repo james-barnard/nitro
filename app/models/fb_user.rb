a1173eb243233479eb298d09886f6f2715b06d94
James Barnard <jwbarnard@comcast.net>
2017-01-24 13:09:07 -0600

    adding not_located to fb_user

--- app/models/fb_user.rb
+++ app/models/fb_user.rb
@@ -1,8 +1,13 @@
 class FbUser < ActiveRecord::Base
   has_many :parts
   CONVERSATION_TIMEOUT=360
+  LOCATION_TIMEOUT=900
 
   def ungreeted?
     (Time.now - parts.last.created_at) > CONVERSATION_TIMEOUT
+  end
+
+  def not_located?
+    (Time.now - parts.where(type: "location").created_at) > LOCATION_TIMEOUT
   end
 end
